c3cd6e9099626d8e55509dc3c1aae7b42bf663f8
Adithya Dixit <adithyadixit2522@gmail.com>
2025-03-05 11:21:43 +0530

    Modified admin.py to customise admin display

--- django_local_library/catalog/admin.py
+++ django_local_library/catalog/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book, BookInstance, Genre, Author, Language
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+admin.site.register(Genre)
+# admin.site.register(Author)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+print("Current registered models:")
+for model, model_admin in admin.site._registry.items():
+    print(model.__name__)
